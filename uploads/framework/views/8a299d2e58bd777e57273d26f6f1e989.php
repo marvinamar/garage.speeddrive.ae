29deee32e33dddc7189e700e968584d91ba63146
marvin <marvin@amarcp.ae>
2023-05-26 10:21:39 +0400

    Update

--- uploads/framework/views/8a299d2e58bd777e57273d26f6f1e989.php
+++ uploads/framework/views/8a299d2e58bd777e57273d26f6f1e989.php
@@ -1,4 +1,4 @@
-<?php global $s_v_data, $user, $title, $widgets, $projects, $tasks, $income; ?>
+<?php global $s_v_data, $user, $title, $inventory, $suppliers; ?>
                 <div class="nk-header nk-header-fixed is-light">
                     <div class="container-fluid">
                         <div class="nk-header-wrap">
